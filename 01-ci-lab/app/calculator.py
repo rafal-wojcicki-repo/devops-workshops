23ea9b4fa3688907a39c5cd0ba7d150a83fb0bfe
Rafał Wójcicki <rafal15100@gmail.com>
2026-06-20 15:51:21 +0200

    feat: add lambda_handler function for handling arithmetic operations

--- 01-ci-lab/app/calculator.py
+++ 01-ci-lab/app/calculator.py
@@ -3,3 +3,26 @@ def add(a, b):
 
 def divide(a, b):  
     return a / b
+
+def lambda_handler(event, context):
+    if "body" in event:
+        if isinstance(event['body'], str):
+            import json
+            body = json.loads(event['body'])
+
+
+    a = body['a']
+    b = body['b']
+    operation = body['operation']
+
+    if operation == 'add':
+        result = add(a, b)
+    elif operation == 'divide':
+        result = divide(a, b)
+    else:
+        raise ValueError("Unsupported operation")
+
+    return {
+        'statusCode': 200,
+        'body': result
+    }
